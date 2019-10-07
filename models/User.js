2ad82a7ba134e622aa1309a48fe04263bb000ac8
Restyled.io <commits@restyled.io>
2019-10-07 11:00:30 +0000

    Restyled by prettier

--- models/User.js
+++ models/User.js
@@ -1,5 +1,5 @@
 var mongoose = require("mongoose");
-var bcrypt = require('bcrypt');
+var bcrypt = require("bcrypt");
 var UserSchema = mongoose.Schema({
   name: {
     type: String,
@@ -11,38 +11,36 @@ var UserSchema = mongoose.Schema({
   }
 });
 
-
 //authenticate input against database
-UserSchema.statics.authenticate = function (name, password, callback) {
-    User.findOne({ name: name })
-        .exec(function (err, user) {
-            if (err) {
-                return callback(err)
-            } else if (!user) {
-                var err = new Error('User not found.');
-                err.status = 401;
-                return callback(err);
-            }
-            bcrypt.compare(password, user.password, function (err, result) {
-                if (result === true) {
-                    return callback(null, user);
-                } else {
-                    return callback();
-                }
-            })
-        });
-}
+UserSchema.statics.authenticate = function(name, password, callback) {
+  User.findOne({ name: name }).exec(function(err, user) {
+    if (err) {
+      return callback(err);
+    } else if (!user) {
+      var err = new Error("User not found.");
+      err.status = 401;
+      return callback(err);
+    }
+    bcrypt.compare(password, user.password, function(err, result) {
+      if (result === true) {
+        return callback(null, user);
+      } else {
+        return callback();
+      }
+    });
+  });
+};
 
 //hashing a password before saving it to the database
-UserSchema.pre('save', function (next) {
-    var user = this;
-    bcrypt.hash(user.password, 10, function (err, hash) {
-        if (err) {
-            return next(err);
-        }
-        user.password = hash;
-        next();
-    })
+UserSchema.pre("save", function(next) {
+  var user = this;
+  bcrypt.hash(user.password, 10, function(err, hash) {
+    if (err) {
+      return next(err);
+    }
+    user.password = hash;
+    next();
+  });
 });
 
 module.exports = User = mongoose.model("User", UserSchema);
